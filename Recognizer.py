197c6f5da46b7404570036c4f83edf0b525786b0
atrukhanov <>
2020-08-20 20:22:07 +0300

    create Recognizer class with structure

---
+++ Recognizer.py
@@ -0,0 +1,10 @@
+
+
+class Recognizer(object):
+
+	def __init__(self, channel: str):
+		self.channel = channel
+		self.buffer = []
+
+	def check_message(self, message): #To Do: create procedure of analyze
+		pass
