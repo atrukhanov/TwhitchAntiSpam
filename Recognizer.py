bdab33df744f14c30cd141cf908d04f09e690817
atrukhanov <>
2020-08-21 20:32:09 +0300

    add v.1 edition

--- Recognizer.py
+++ Recognizer.py
@@ -1,10 +1,30 @@
-
+from Analyzer import check_first_word, check_bad_encoding
 
 class Recognizer(object):
 
 	def __init__(self, channel: str):
-		self.channel = channel
-		self.buffer = []
+		self.__channel = channel
+		self.__message_buffer = []
+		self.__message_flags = []
 
-	def check_message(self, message): #To Do: create procedure of analyze
-		pass
+	def check_message(self,  message: str) -> bool:
+		return check_message(message) and check_bad_encoding(message)
+
+	def push_buffer(self, message):
+		self.__message_buffer.append(message)
+		self.__message_flags.append(False)
+
+	def check_matching(self, message: str):
+		for i in range(len(self.__message_buffer)):
+			if self.__message_buffer[i][6:] == message[6:]:
+				return i
+		return -1
+
+	def get_message(self, id: int):
+		return self.__message_buffer[id]
+
+	def check_flag(self, id: int) -> bool:
+		return self.__message_flags[id]
+
+	def set_ban(self, id: int):
+		self.__message_flags[id] = True
--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 
+from twitchio.ext import commands
+import Recognizer
 
 
-def main():
-	pass
+class Bot(commands.Bot):
+
+	recognizer = Recognizer('ipsum')
+
+	def __init__(self):
+		super().__init__(irc_token='...', client_id='...', nick='...', prefix='!',
+						 initial_channels=['...'])
+
+	async def event_message(self, message):
+		if recognizer.check_message(message.content):
+			id = recognizer.check_matching(message.content)
+			if (id == -1):
+				recognizer.push_buffer(message)
+			else:
+				if !check_flag(id):
+					message.channel.ban(recognizer.get_message(id).author)
+					set_ban(id)
+				message.channel.ban(message.author)
+
+if __name__ == '__main__':
+
+	bot = Bot()
+	bot.run()
