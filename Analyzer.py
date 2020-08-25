43faa75bdf3734b165110e30c58726e6123412fb
Alex <iampopovich@gmail.com>
2020-08-25 17:50:22 +0300

    Update Analyzer.py
    - global variables replaced for check_bad_encoding method default arguments
    - check_first_word returns bool value much laconic

--- Analyzer.py
+++ Analyzer.py
@@ -1,14 +1,12 @@
 import re
 
 expression = r'[a-zA-Z0-9]{6}'
-treshold = 2
-max_index = 1280 # Todo: choose code
 
 def check_first_word(message: str) -> bool:
 	word = message.split(' ')[0]
-	return True if re.match(expression, word) else False
+	return bool(re.match(expression, word))
 
-def check_bad_encoding(message: str) -> bool:
+def check_bad_encoding(message: str, max_index = 1280, treshold = 2) -> bool:
 	counter = 0
 	for symbol in message:
 		if counter == treshold:
