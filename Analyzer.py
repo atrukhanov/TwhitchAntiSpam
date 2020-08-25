b3031342845e1cb0ebcb123898f45da44c4a9a54
Alex <iampopovich@gmail.com>
2020-08-26 02:55:01 +0300

    counter variable removed
    now check_bad_encoding method needs no counter. we decrease treshold value each time when suspicious symbol was found. And then, when it become equal zero we'll return value True.

--- Analyzer.py
+++ Analyzer.py
@@ -7,9 +7,8 @@ def check_first_word(message: str) -> bool:
 	return bool(re.match(expression, word))
 
 def check_bad_encoding(message: str, max_index = 1280, treshold = 2) -> bool:
-	counter = 0
 	for symbol in message:
-		if counter == treshold:
+		if !treshold:
 			return True
-		counter += 1 if int(ord(symbol)) > max_index else 0
+		treshold -=1 if int(ord(symbol)) > max_index else None
 	return False
