9c24822eb362f5986819a9be5950c810d741a91e
Alexander <54072529+atrukhanov@users.noreply.github.com>
2020-09-10 03:37:02 +0300

    fix issue

--- Analyzer.py
+++ Analyzer.py
@@ -4,7 +4,7 @@ expression = r'[a-zA-Z0-9]{6}'
 
 def check_first_word(message: str) -> bool:
 	word = message.split(' ')[0]
-	return bool(re.match(expression, word))
+	return bool(re.match(expression, word)) and len(word) == 4
 
 def check_bad_encoding(message: str, max_index = 1280, treshold = 2) -> bool:
 	for symbol in message:
